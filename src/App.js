bd817911b2d1962233e0c643477e2ddfd4c961d1
Jeffrey Gil P. Deimos <jeffroxgil@gmail.com>
2022-08-10 21:13:33 +0800

    Last commit.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Fibonacci from './pages/Fibonacci';
+import Strings from './pages/Strings';
+import PrimeNumbers from './pages/PrimeNumbers';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Fibonacci />} />
+        <Route path='/strings' element={<Strings />} />
+        <Route path='/prime-numbers' element={<PrimeNumbers />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
